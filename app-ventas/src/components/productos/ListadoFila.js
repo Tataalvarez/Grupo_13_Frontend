97ba61b02b4f3a02738183322cb4f765b0640245
Paola Andrea Domínguez Vélez <paolala1017@gmail.com>
2021-10-02 16:20:34 -0500

    Agrego a listado información de las columnas y creo archivo ListadoFila con sus respectivas filas

---
+++ app-ventas/src/components/productos/ListadoFila.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const TablaFila = ({ el, setDataToEdit, deleteData }) => {
+  let {
+    id,
+    descripcion,
+    valor,
+    estado,
+    acciones,
+  } = el;
+  return (
+    <>
+      <tr>
+        <td>{id}</td>
+        <td>{descripcion}</td>
+        <td>{valor}</td>
+        <td>{estado}</td>
+        <td>{acciones}</td>
+        <td>
+          <button
+            className="btn btn-primary btn-sm"
+            onClick={() => setDataToEdit(el)}
+          >
+            Editar
+          </button>
+          <button
+            className="btn btn-danger btn-sm"
+            onClick={() => deleteData(id)}
+          >
+            Eliminar
+          </button>
+        </td>
+      </tr>
+    </>
+  );
+};
+
+export default ListadoFila;
