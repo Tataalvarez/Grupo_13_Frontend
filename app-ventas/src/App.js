d065cb925c5b5f9867f25fd52710fc943df2118d
Richar Mora <richmon430@gmail.com>
2021-10-02 02:55:12 -0500

    Moficando tamano de fuente

--- app-ventas/src/App.js
+++ app-ventas/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import "./App.css";
+
+// Componentes
+import Pedidos from "./components/ventas/Pedidos";
+//import Formulario from "./components/pedidos/Formulario";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-4">
+      <div className="row">
+        <Pedidos />
+      </div>
     </div>
   );
-}
+} 
 
 export default App;
