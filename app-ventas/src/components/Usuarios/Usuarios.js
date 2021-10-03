47d9309df1cfb79c1762171ce9efc5b146066707
Richar Mora <richmon430@gmail.com>
2021-10-02 19:30:05 -0500

    Actualizando plantilla

--- app-ventas/src/components/Usuarios/Usuarios.js
+++ app-ventas/src/components/Usuarios/Usuarios.js
@@ -1,36 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const Usuarios = ({ data, setDataToEdit, deleteData }) => {
   return (
     <div className="container">
-      <div className="mt-5 text-center">
-      <h3>GESTIÓN DE USUARIOS</h3>
-      <table class="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">Id</th>
-      <th scope="col">Usuario</th>
-      <th scope="col">Rol</th>
-      <th scope="col">Estdo</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Vendedor</td>
-      <td>Autorizado</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Administrador</td>
-      <td>Pendiente</td>
-    </tr>
-  </tbody>
-</table>
-    </div>
+      <div className="card">
+        <div className="card-body">
+          <div className="text-center">
+            <h3>GESTIÓN DE USUARIOS</h3>
+            <table class="table table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">Id</th>
+                  <th scope="col">Usuario</th>
+                  <th scope="col">Rol</th>
+                  <th scope="col">Estdo</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark</td>
+                  <td>Vendedor</td>
+                  <td>Autorizado</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Jacob</td>
+                  <td>Administrador</td>
+                  <td>Pendiente</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
-export default Usuarios
+export default Usuarios;
