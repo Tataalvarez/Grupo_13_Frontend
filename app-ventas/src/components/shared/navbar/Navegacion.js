4cef33c308b656c8416f461c850d413c58f4c3ae
Richar Mora <richmon430@gmail.com>
2021-10-07 16:05:40 -0500

    actualizando home y navbar

---
+++ app-ventas/src/components/shared/navbar/Navegacion.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Container, Nav, Navbar } from "react-bootstrap";
+//import { Link } from 'react-router-dom';
+
+const Navegacion = () => {
+  return (
+    <Navbar className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <Container>
+      <Navbar.Brand href="/">Grupo 13</Navbar.Brand>
+      <Nav className="me-center">
+        <Nav.Link href="/productos">Productos</Nav.Link>
+        <Nav.Link href="/ventas">Ventas</Nav.Link>
+        <Nav.Link href="/admin" eventKey="disabled">Administracion</Nav.Link>
+      </Nav>
+      <Nav>
+        <Nav.Link href="/login">LogIn</Nav.Link>
+        <Nav.Link eventKey={2} href="/registro">
+          Registro
+        </Nav.Link>
+      </Nav>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Navegacion;
